1512078359aa851226b8b0d4f471e6ab2a5187d8
jdelmundo23 <joshdelm@gmail.com>
2025-05-21 11:01:38 -0400

    add PATCH method to CORS config

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,7 +23,7 @@ import ipsRouter from "./routes/api/network/ips";
 
 const corsOptions = {
   origin: process.env.CLIENT_ORIGIN,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
 
