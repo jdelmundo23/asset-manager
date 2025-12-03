ac19e74c6a4a9dbc747163c87834715398cacdf5
jdelmundo23 <joshdelm@gmail.com>
2025-12-03 16:13:36 -0500

    error handling changes

--- client/src/lib/handleError.ts
+++ client/src/lib/handleError.ts
@@ -1,38 +1,39 @@
 import axios from "axios";
 import { showErrorToast, showSessionExpiredToast } from "./toasts";
 
+const defaultErrorMessages: Record<number, string> = {
+  400: "Bad request",
+  401: "Session expired or invalid",
+  403: "Access denied",
+  500: "Server error",
+};
+
 export function handleError(error: unknown, loadingToastId?: string | number) {
   const toastId = loadingToastId ?? "error";
+
   if (axios.isAxiosError(error)) {
     const status = error.response?.status;
-    const errorMsg = error.response?.data?.error;
 
     if (!status) {
-      showErrorToast(toastId, "Network error or server is unreachable.");
-    } else if (status === 400) {
-      showErrorToast(toastId, errorMsg);
-    } else if (status === 401) {
-      showSessionExpiredToast(toastId);
-    } else if (status === 403) {
-      showErrorToast(
-        toastId,
-        "You don't have permission to perform this action."
-      );
-    } else if (status === 500) {
-      showErrorToast(
-        toastId,
-        errorMsg ?? "Server error. Please try again later."
-      );
-    } else {
-      showErrorToast(toastId, `Unexpected error (${status}).`);
+      const networkErrorMsg = "Network error or server unreachable";
+      showErrorToast(toastId, networkErrorMsg);
+      return networkErrorMsg;
     }
 
-    return errorMsg || "Unexpected error";
+    const errorMsg =
+      error.response?.data?.error ||
+      defaultErrorMessages[status] ||
+      "Unexpected error";
+
+    if (status === 401) {
+      showSessionExpiredToast(toastId);
+    } else {
+      showErrorToast(toastId, errorMsg);
+    }
+
+    return errorMsg;
   } else {
-    console.error("Unexpected error:", error);
-    showErrorToast(
-      toastId,
-      "An unexpected error has occurred. Please check the console for more details."
-    );
+    console.error("Unexpected error");
+    showErrorToast(toastId, "An unexpected error has occurred");
   }
 }
